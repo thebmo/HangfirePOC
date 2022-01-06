e3d25189666a7b1c391226795ceda5ee8772b6f2
Brian Mosier <bmoser@gmail.com>
2022-01-06 18:19:14 -0500

    real first commit

---
+++ CacheRefreshService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HangfirePOC
+{
+    public class CacheRefreshService : BackgroundService
+    {
+        private readonly IBackgroundJobClient _backgroundJobs;
+        private readonly ILogger<CacheRefreshService> _logger;
+        private readonly IMemoryCache _cache;
+
+        private const string DEFAULT_Q = "default";
+        private const string KEY = "test_key";
+
+        public static string[] QUEUES = { DEFAULT_Q };
+
+
+        public CacheRefreshService(
+            IBackgroundJobClient backgroundJobs,
+            IMemoryCache cache,
+            ILogger<CacheRefreshService> logger)
+        {
+            _backgroundJobs = backgroundJobs ?? throw new ArgumentNullException(nameof(backgroundJobs));
+            _cache = cache;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            _backgroundJobs.Enqueue(() => initalSetCache());
+
+            RecurringJob.AddOrUpdate(
+                    DEFAULT_Q,
+                    () => CheckOrRefreshCache(),
+                    Cron.Minutely);
+
+            return Task.CompletedTask;
+        }
+
+
+        [Queue(DEFAULT_Q)]
+        public void initalSetCache()
+        {
+            SetCache();
+        }
+
+        [Queue(DEFAULT_Q)]
+        public void CheckOrRefreshCache()
+        {
+            if (!_cache.TryGetValue(KEY, out DateTime then))
+            {
+                _logger.LogError("CACHE NOT FOUND, REFRESHING!");
+                SetCache();
+            }
+            else
+            {
+                _logger.LogCritical($"CACHE FOUND: {then}. REFRESHING!!!");
+                SetCache();
+            };
+        }
+
+        private void SetCache()
+        {
+            var now = DateTime.UtcNow;
+            _logger.LogWarning($"setting cache time to: {now}");
+            _cache.Set(KEY, now);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangfirePOC
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+
+            services.AddHangfire(s => s
+                .UseIgnoredAssemblyVersionTypeResolver()
+                .UseInMemoryStorage());
+
+            services.AddHangfireServer(opt => 
+                opt.Queues = CacheRefreshService.QUEUES);
+
+            services.AddHostedService<CacheRefreshService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
